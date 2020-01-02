99395d2882501083e256fc613e69b832071cc1f2
Janusz Hatala <janusz.hatala@capsilon.com>
2020-01-02 23:28:45 +0100

    Initial commit

---
+++ start-wiremock-ex.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker rm wiremock-ex
+
+docker run -dit --restart unless-stopped --name wiremock-ex \
+  -p 8090:8080 \
+  -v $PWD/stubs:/home/wiremock \
+  -v $PWD/extensions:/var/wiremock/extensions \
+  rodolpheche/wiremock \
+  --extensions com.opentable.extension.BodyTransformer \
+  --verbose \
+  --global-response-templating
+  
